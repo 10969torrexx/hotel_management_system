ff43df3fa6bc7569980d8e3d56ff6a6af37c5a18
10969torrexx <83467428+10969torrexx@users.noreply.github.com>
2024-05-23 07:46:28 +0800

    enable delete room

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -99,7 +99,7 @@
                                             </button>
                                             <div class="dropdown-menu">
                                             <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
-                                            <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
+                                            <a class="dropdown-item" href="{{ route('roomsDelete', ['id' => $item->id]) }}"><i class="bx bx-trash me-1"></i> Delete</a>
                                             </div>
                                         </div>
                                     </th>
