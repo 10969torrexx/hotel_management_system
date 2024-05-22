fb232b9b53a61020a4ded31e02e65bf5678abdcb
10969torrexx <83467428+10969torrexx@users.noreply.github.com>
2024-05-23 07:41:23 +0800

    refactor room view

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -56,49 +56,61 @@
                     </form>
                 </div>
             </div>
-            <div class="card">
-                <h5 class="card-header">List of Rooms</h5>
-                <div class="card-body">
-                  <div class="table-responsive text-nowrap">
-                    <table class="table table-bordered table-hover">
-                      <thead>
-                        <tr>
-                          <th>Room #</th>
-                          <th>Description</th>
-                          <th>Image</th>
-                          <th>Type</th>
-                          <th>Status</th>
-                          <th>Date Added</th>
-                          <th>Actions</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        @foreach ($rooms as $item)
-                            <tr>
-                                <td>{{ $item->number }}</td>
-                                <td class="text-wrap">{{ $item->description }}</td>
-                                <td><img src="/{{ $item->file_path }}" alt="" height="100"></td>
-                                <td>{{ config('const.room_type.'.($item->type) -1 ) }}</td>
-                                <td>{{ config('const.room_status.' . $item->status) }}</td>
-                                <td>{{ date('M, d, Y', strtotime($item->created_at)) }}</td>
-                                <th>
-                                    <div class="dropdown">
-                                        <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                                          <i class="bx bx-dots-vertical-rounded"></i>
-                                        </button>
-                                        <div class="dropdown-menu">
-                                          <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
-                                          <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
-                                        </div>
-                                    </div>
-                                </th>
-                            </tr>
-                        @endforeach
-                      </tbody>
-                    </table>
-                  </div>
+            @if (count($rooms) <= 0)
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="card" style="display: flex; justify-content: center; align-items: center;">
+                            <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                            <lottie-player src="https://lottie.host/0a731f25-c465-43fd-84d3-c19915e2f23f/HGDqixWDVY.json" background="##FFFFFF" speed="1" style="width: 40%;" loop autoplay direction="1" mode="normal"></lottie-player>
+                            <h4 class="text-center">No rooms found</h4>
+                        </div>
+                    </div>
                 </div>
-              </div>
+            @else
+                <div class="card">
+                    <h5 class="card-header">List of Rooms</h5>
+                    <div class="card-body">
+                    <div class="table-responsive text-nowrap">
+                        <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                            <th>Room #</th>
+                            <th>Description</th>
+                            <th>Image</th>
+                            <th>Type</th>
+                            <th>Status</th>
+                            <th>Date Added</th>
+                            <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($rooms as $item)
+                                <tr>
+                                    <td>{{ $item->number }}</td>
+                                    <td class="text-wrap">{{ $item->description }}</td>
+                                    <td><img src="/{{ $item->file_path }}" alt="" height="100"></td>
+                                    <td>{{ config('const.room_type.'.($item->type) -1 ) }}</td>
+                                    <td>{{ config('const.room_status.' . $item->status) }}</td>
+                                    <td>{{ date('M, d, Y', strtotime($item->created_at)) }}</td>
+                                    <th>
+                                        <div class="dropdown">
+                                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                            <i class="bx bx-dots-vertical-rounded"></i>
+                                            </button>
+                                            <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                                            <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
+                                            </div>
+                                        </div>
+                                    </th>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                        </table>
+                    </div>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 @endsection
