a8533fe46e5ebc11f1b92568fd3f512ebe0f6234
10969torrexx <83467428+10969torrexx@users.noreply.github.com>
2024-05-22 08:02:54 +0800

    added new templates

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('keto.index');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/sneat', function(){
+    return view('sneat.index');
+});
