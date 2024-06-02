3a5c3b4b6cd9927d6d4e3a1c72c796c94db2dde0
10969torrexx <83467428+10969torrexx@users.noreply.github.com>
2024-06-03 06:14:15 +0800

    enable checking of available rooms based on check in and out

--- app/Models/Rooms.php
+++ app/Models/Rooms.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Reservations;
 class Rooms extends Model
 {
     use HasFactory;
@@ -17,4 +17,9 @@ class Rooms extends Model
         'status',
         'price'
     ];
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservations::class, 'room_id');
+    }
 }
