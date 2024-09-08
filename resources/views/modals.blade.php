49415c494b4663ab054f16fbdc86b5ab3fee2653
10969torrexx <83467428+10969torrexx@users.noreply.github.com>
2024-09-08 21:39:31 +0800

    Add reservation notificaition

---
+++ resources/views/modals.blade.php
@@ -0,0 +1,17 @@
+<div id="extendReservationModal" class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Reservation Notification</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <h3 id="headerMessage"></h3>
+        </div>
+        <div class="modal-footer">
+          <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Extend Reservation?</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Confirm Checkout</button>
+        </div>
+      </div>
+    </div>
+</div>
